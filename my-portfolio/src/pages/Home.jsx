57ce698ab325ce1d8424d521b6b79ad4133a9728
ayly8 <allison.y.ly@gmail.com>
2024-07-24 12:04:15 -0700

    added descriptions for experiences and projects

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -16,7 +16,12 @@ function Home() {
                <div id="pic-and-desc">
                   <img id="about-me-pic" src={ally} alt="picture of Allison Ly" loading="lazy"/>
                   <p id="about-me-para">
-                     {/* Hi my name is Allison Ly, and welcome to my portfolio website! */}
+                  Hi, my name is Allison Ly and welcome to my portfolio website! I am a Computer Science major at Cal Poly Pomona and will be 
+                  graduating in December 2024. In my free time, I enjoy drawing, reading, watching anime, and playing games. I am interested in 
+                  Frontend Development, UI/UX Design, Project Management, and Software Engineering. I have maintained a GPA of 3.67 and made the 
+                  Dean’s Honors List every semester. For almost three years I have been an active member of Cal Poly Pomona’s sheCodes club, which 
+                  supports underrepresented students in STEM. I also participated in the Summer 2023 Unmanned Aerial Vehicle Research Experience 
+                  for Undergrads at Cal Poly Pomona, where I gained interdisciplinary experience.
                   </p>
                </div>
                <div id="connect">
@@ -47,22 +52,39 @@ function Home() {
                <Description 
                   title="Research Experience for Undergrads (REU)"
                   duration="June - August 2023"
-                  desc=""/>
+                  desc="I participated in a National Science Foundation (NSF) funded Research Experience for Undergrads (REU) at Cal Poly Pomona 
+                  as an Unmanned Aerial Vehicle (UAV) research assistant on the 'UAV Fire Detection and Suppression' team. Selected as one of 12 
+                  participants from over 100 applicants, I gained hands-on experience with both hardware and software. The main tasks assigned to 
+                  me were connecting the UAV to the ground control station (GCS) to relay the flight log data, as well as improve the visualization 
+                  of that data. I configured two Digi XBee radio communication devices using XCTU software to enable the UAV to send GPS coordinates 
+                  to the GCS. I utilized NASA's Worldwide Wind Java API with NetBeans to ensure the GCS received and displayed the correct GPS 
+                  coordinates. To enhance data visualization, I created a graphical user interface using Java Swing to display the UAV flight log data. 
+                  At the end of the program, I co-authored a final report with my team discussing our literature review, objectives, methodology, and 
+                  results for research paper."/>
                <Description 
                   title="Aims Academy"
                   duration="July - December 2022"
-                  desc=""/>
+                  desc="For half a year I worked as a teacher at Aims Academy, located in Walnut CA. During the Summer, I managed 10 students in a combination 
+                  class of 2nd-3rd graders, and in the school semester I managed 15 students in a combination class of K-1st graders. I taught Math and English 
+                  to students in various learning stages using common core methods, and provided homework assistance. Additionally, I tutored Pre-Algebra and 
+                  Algebra 1 weekly during the semester."/>
             </div>
             <div id="extra-exp">
                <h1 className="section-title">⋆ ˚｡⋆୨୧˚ Extracurricular ˚୨୧⋆｡˚ ⋆</h1>
                <Description 
                   title="Cal Poly Pomona sheCodes Internal Vice President"
                   duration="May 2023 - May 2024"
-                  desc=""/>
+                  desc="As Internal Vice President, I was in charge of our two annual professional events, our Semester Long Project (SLP) Program during the Fall 
+                  and our Future Woman Developer’s (FWD) Conference. I planned and organized SLP 2023 to offer underrepresented students team project opportunities 
+                  that allow them to explore CS topics outside of class. During 9 weeks of SLP, I oversaw the development of 8 teams and facilitated Demo Day. I 
+                  directed and hosted the FWD 2024 conference, introducing 25 middle/high school female and minority students to various CS topics. I provided students 
+                  with 3 hands-on workshops about Web Development, Python, and Cybersecurity, as well as a Minority in Tech panel."/>
                <Description 
                   title="Cal Poly Pomona sheCodes Social Chair"
                   duration="May 2023 - May 2024"
-                  desc=""/>
+                  desc="As Social Chair, I was in charge of planning and hosting monthly socials, executive board retreats, and our End of the Year banquet for over 50 members.  
+                  Each social event aimed to foster an inclusive and supportive environment for women and minorities in STEM. Moreover, the executive board retreats focused on 
+                  building teamwork skills for the school year, while the banquet celebrated the accomplishments and achievements of sheCodes throughout the year."/>
             </div>
             <SeeMore 
                first_location="/work"
--- my-portfolio/src/pages/Work.jsx
+++ my-portfolio/src/pages/Work.jsx
@@ -11,48 +11,64 @@ function Work() {
                <Project 
                   title="Portfolio Website"
                   briefDesc="React Web Application"
-                  link=""
+                  link="https://github.com/ayly8/Portfolio"
                   linkName="Github"
-                  fullDesc="test"
+                  fullDesc="Designed and implemented a portfolio website to showcase all of my experiences, projects, and artwork. 
+                  Utilized React to facilitate building a component based website. Used Figma to create prototype designs of the website. 
+                  Deployed the website through GitHub Pages."
                />
                <Project 
                   title="Restaurant Delivery System"
                   briefDesc="Java Desktop Application"
-                  link=""
+                  link="https://github.com/CS3560-02-07/Restaurant-Delivery-System"
                   linkName="Github"
-                  fullDesc=""
+                  fullDesc="Collaborated with a team of 5 to create a Java desktop application, allowing restaurants to request orders to be 
+                  delivered and drivers to pick-up those orders. Depending on the user, after logging in they would be presented with a specified 
+                  user interface for restaurants or drivers. Conceptualized the graphical user interface (GUI) by using Figma and then utilized Java 
+                  Swing to integrate GUI components: login, account registration, profiles for restaurant owners and delivery drivers. "
                />
             </div>
             <div className="proj-section">
                <Project 
                   title="Planning Pal"
-                  briefDesc="Fullstack Web Application"
-                  link=""
+                  briefDesc="Full Stack Web Application"
+                  link="https://github.com/ayly8/Planning-Pattern"
                   linkName="Github"
-                  fullDesc=""
+                  fullDesc="Full stack web application that is designed to help users plan anything or to help them get started with planning if 
+                  they are stuck. Users answer 3 questions regarding what they want to plan and those answers are used as input. Leveraged the 
+                  ChatGPT API to generate a suggested plan based on the inputs. Used Flask to aid the process of retrieving user inputs for the 
+                  ChatGPT API to generate the output. Constructed the website using HTML, CSS, and JavaScript."
                />
                <Project 
                   title="NomNomMap"
                   briefDesc="Android Mobile Application"
-                  link=""
+                  link="https://github.com/yuunster/NomNomMap"
                   linkName="Github"
-                  fullDesc=""
+                  fullDesc="Collaborated with a team of 3 to create an Android mobile application, utilizing the Google Maps API where users can login, 
+                  store information about meals based on their location, and view their history of logged meals. Used Figma to create a prototype version 
+                  of our mobile application. Enhanced user experience by implementing a bottom navigation bar and camera feature in Kotlin. Constructed the 
+                  user interface for: Map, Meal Detail, Profile, and Settings, using Android Studio's Layout Editor to assemble components."
                />
             </div>
             <div className="proj-section">
                <Project 
-                  title="ChatGPT Senior Project"
+                  title="ChatGPT Social Bias Senior Project"
                   briefDesc="Data Analysis"
-                  link=""
-                  linkName="Github"
-                  fullDesc=""
+                  link="https://drive.google.com/file/d/1w2c8bP2h33B-bFoUmRG3SfAeJIp45loA/view?usp=sharing"
+                  linkName="Poster"
+                  fullDesc="Evaluatedpotential gender and racial bias in ChatGPT. Crafted prompts on various scenarios that may produce bias based on the 
+                  gender identity and ethnic background of the names provided. Conducted iterative testing with prompt variations to interpret the results, 
+                  generating a dataset for future analysis. Co-authored a research poster with my team discussing our results for research symposium."
                />
                <Project 
                   title="Slime Hop"
                   briefDesc="2D Godot Puzzle Platform Game"
-                  link=""
-                  linkName="Github"
-                  fullDesc=""
+                  link="https://piggyfriend.itch.io/slime-hop"
+                  linkName="itch.io"
+                  fullDesc="Collaborated with a team of 7 to create a 2D puzzle platform game. Users play as a slime that can split into multiple copies using a 
+                  Copy and Swap mechanic to traverse through various obstacles. Produced two captivating background illustrations for the menu and credits artwork 
+                  using MediBang Paint illustration software. Developed the menu scene and user interface for menu buttons in Godot, strategically designed to 
+                  captivate players’ interest."
                />
             </div>
             <h1 className="section-title">⋆⁺₊⋆₊⁺ Hackathons ⋆⁺₊⋆₊⁺</h1>
@@ -60,16 +76,19 @@ function Work() {
                <Project 
                   title="Nutriversity"
                   briefDesc="Wafflehacks 2023 - Web Application"
-                  link=""
+                  link="https://devpost.com/software/nutriversity"
                   linkName="Devpost"
-                  fullDesc=""
+                  fullDesc="Collaborated in a team of 2 to create a website within 24 hours. Website allows users to log and store nutritional info 
+                  onto a database for them to reference at any time. Helped develop the frontend of our website, utilizing HTML and CSS. "
                />
                <Project 
                   title="AnimEats"
                   briefDesc="AthenaHacks 2023 - Web Application"
-                  link=""
+                  link="https://devpost.com/software/animeats"
                   linkName="Devpost"
-                  fullDesc=""
+                  fullDesc="Collaborated with a team of 4 to create a website within 24 hours. Website allows users to click on an anime character 
+                  to see what their favorite food recipe is and they can use the search food feature to find any food item. Helped develop the frontend 
+                  of our website, utilizing HTML and CSS. "
                />
             </div>
             <SeeMore 
