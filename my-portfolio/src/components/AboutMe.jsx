7ed972b1c59b5d33b9e53434f6a7a7d276514177
ayly8 <allison.y.ly@gmail.com>
2026-05-05 14:13:49 -0700

    fixed about me

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ import insta from "../assets/instagram.png"
 function AboutMe() {
    return (
       <div className="border-2 border-[#dd127f] rounded-[10px] shadow-[5px_5px_5px_#FBACBE] bg-white p-[15px] m-5 static
-         md:fixed md:top-[55px] md:w-[300px] md:h-[calc(95vh-55px)] md:overflow-y-auto">
+         md:fixed md:top-[55px] md:w-[300px] md:h-[calc(85vh-55px)]">
          <img className="w-24 h-24 rounded-full mb-2" src={ally} alt="picture of Allison Ly" loading="lazy" />
          <h2>Allison Y. Ly</h2>
          <h3>Digital Engineering Tech Consultant at EY</h3>
@@ -19,8 +19,8 @@ function AboutMe() {
          <p>Computer Science | Dec 2024</p>
          <hr></hr>
          <p>
-            I have a background in teaching STEAM concepts, working in collaborative settings, and I am passionate about applying 
-            these skills in a professional tech environment. I enjoy spending time with loved ones, doing art, and playing games in my free time!
+            I have a background in teaching STEAM concepts, working in collaborative settings, and I am passionate about applying
+            these skills in a professional tech environment.
          </p>
          <div className="flex gap-5 mt-3">
             <a href="https://docs.google.com/document/d/1sOQ6vM8wcC0NNTz2ZSJfEKxyDb9MASPyLu3Si5iHMpg/edit?usp=sharing" target="_blank">
